5ff908bedfc47b290f4620592f258b1e8ae16c0e
Rifsan <115974355+Rifsan@users.noreply.github.com>
2025-06-15 20:15:41 +0800

    Update dbban .sql

--- dbban (4).sql
+++ dbban (4).sql
@@ -1,31 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Jun 15, 2025 at 12:02 PM
--- Server version: 8.4.3
--- PHP Version: 8.3.16
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dbban`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `alamat_user`
---
 
 CREATE TABLE `alamat_user` (
   `idAlamat` int NOT NULL,
